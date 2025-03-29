0de67a9a6e1b7dea24b0306ac1b0145cbf47f968
Natchanon nabboon <nbmxrk@gmail.com>
2025-03-29 16:45:49 +0700

    begin create favourite page

--- intrn/lib/main.dart
+++ intrn/lib/main.dart
@@ -1,122 +1,156 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData(useMaterial3: true),
+      home: NavigationBars(),
+    );
+  }
+}
+class NavigationBars extends StatefulWidget {
+  const NavigationBars({super.key});
+
+  @override
+  State<NavigationBars> createState() => NavigationBarState();
+}
+
+class NavigationBarState extends State<NavigationBars>{
+  // const NavigationBars({super.key});
+  int pageindex = 0 ;
+  List<String> titles = <String>['Favourite', 'In progess', 'Applied'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          labelTextStyle: WidgetStateProperty .resolveWith<TextStyle>((
+            Set<WidgetState> states,
+          ) {
+            if (states.contains(WidgetState.selected)) {
+              return TextStyle(color: Colors.deepOrangeAccent);
+            }else{
+              return TextStyle(color: Colors.black);
+            }
+          }),
+        ),
+    child: NavigationBar(
+      backgroundColor: Color.fromARGB(255, 243, 243, 243),
+      selectedIndex: pageindex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          pageindex = index;
+        });
+      },
+      destinations: [
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined, color: pageindex == 0 ? Colors.deepOrangeAccent : null),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.list_alt_outlined, color: pageindex == 1 ? Colors.deepOrangeAccent : null),
+          label: 'List',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person_outlined, color: pageindex == 2 ? Colors.deepOrangeAccent : null),
+          label: 'Profile',
+        ),
+      ],
+    ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: appbars(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class appbars extends StatefulWidget {
+  const appbars({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<appbars> createState() => AppbarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AppbarState extends State<appbars> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3, 
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.deepOrangeAccent,
+            title: Text(
+              'Job List',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ],
+          
+          ),
+          body: Column(
+            children: [
+              Container(
+                color: const Color.fromARGB(255, 235, 235, 235), 
+                child: TabBar(
+                  indicatorColor: Colors.deepOrangeAccent,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.black,
+                  tabs: [
+                    Tab(text: 'Favourite'),
+                    Tab(text: 'In progress'),
+                    Tab(text: 'Applied'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: TabBarView(
+                  children: [
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.add_box_outlined, 
+                              size: 70.0, 
+                              color: Colors.grey, 
+                            ),
+                            SizedBox(
+                              height: 8.0,
+                            ), 
+                            Text(
+                              "Don't have any applications.", 
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Center(child: Text('In progress')),
+                      Center(child: Text('Applied')),
+                    ],
+                ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
