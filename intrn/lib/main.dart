1ac550b62c6a96126252439a0015c2ef5eb79d70
RapeeSu <rapeemark@gmail.com>
2025-04-11 16:35:20 +0700

    Add files via upload
    Profile and Editer for Profile page

--- intrn/lib/main.dart
+++ intrn/lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'models/user.dart';
+import 'pages/profile_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final User testUser = User(
+    firstName: "John",
+    lastName: "Doe",
+    birthDate: "27/08/1996",
+    phone: "099-999-9999",
+    biography: "Senior UX/UI Designer",
+    address1: "Address 1",
+    address2: "Address 2",
+    city: "Bangkok",
+    country: "Thailand",
+    university: "Mahidol University",
+    faculty: "Information Communication and",
+    degree: "Bachelor's Degree",
+    startDate: "01/01/2015",
+    endDate: "01/01/2019",
+    email: "john.doe@who.com",
+  );
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: ProfilePage(user: testUser),
     );
   }
 }
